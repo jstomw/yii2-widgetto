4faf8c9a46483fe59fe464696d9b33283ded990d
exru <exerus0@gmail.com>
2018-03-04 00:56:18 +0300

    FIX, shield json quotes (refactoring)

--- Widgetto.php
+++ Widgetto.php
@@ -11,19 +11,19 @@ class Widgetto extends Widget
 {
 
     /**
-     * Parsing html
+     * HTML text
      * @var string
      */
     public $html = '';
 
     /**
-     * Begin of template
+     * Begin tag of template
      * @var string
      */
     public $beginTag = "\[\[";
 
     /**
-     * End of template
+     * End tag of template
      * @var string
      */
     public $endTag = "\]\]";
@@ -45,52 +45,84 @@ class Widgetto extends Widget
     public $widgets = [];
 
     /**
-     * Hide wrong widgets
+     * Decode HTML entities before extract json params
+     * For example:
+     * //{"text":"&lt;h4 style="float:right;"&gt;foo text&lt;/h4&gt;"}
+     * //...it will be translate as...
+     * //{"text":"<h4 style="float:right;">foo text</h4>"}
      * @var bool
      */
-    public $escapeWrongResult = true;
+    public $htmlEntitesDecode = true;
 
-    private function _replaceWidgets()
+    /**
+     * @param $w_name string Widget name
+     * @param $item string peace of text
+     * @return array|mixed
+     */
+    public function extractJsonParams($w_name, $item)
     {
-        foreach ($this->widgets as $parsing_name => $options) {
-            $this->html = preg_replace_callback("/{$this->beginTag}$parsing_name.*?{$this->endTag}/s", function ($match) use ($parsing_name) {
-                $peace = current($match);
-                $widget_params = ArrayHelper::getValue($this->widgets, $parsing_name);
-                try {
-                    $options = [];
-                    $json = trim(preg_replace(["/{$this->beginTag}$parsing_name/s", "/{$this->endTag}/s"], '', $peace));
-                    try {
-                        if ($json = Json::decode($json)) {
-                            $options = $json;
-                        }
-                    } catch (Exception $e) {}
-                    if (gettype($widget_params) == 'array') {
-                        $widget_params = ArrayHelper::merge($widget_params, $options);
-                        if ($data = \Yii::createObject($widget_params)) {
-                            return $data->run();
-                        }
-                    }
-                    if (gettype($widget_params) == 'string') {
-                        return $widget_params;
-                    }
-                    if ($widget_params instanceof \Closure) {
-                        return call_user_func($widget_params, $parsing_name, $options);
-                    }
-
-                } catch (Exception $e) {
-                    if (!$this->escapeWrongResult && YII_DEBUG == true) {
-                        var_dump($e);
-                        exit;
-                    }
-                    return $peace;
+        $options = [];
+        //remove first & last tags or clear invalid json
+        if ($j_params = trim(preg_replace(["/{$this->beginTag}$w_name/s", "/{$this->endTag}/s"], '', $item))) {
+            if ($this->htmlEntitesDecode) {
+                $j_params = html_entity_decode($j_params);
+                $j_params = preg_replace('/(<[^>]+\=)"([^"]+)">/s', '$1\"$2\">', $j_params);
+            }
+            try { //don't handle with exception
+                if ($p_params = Json::decode($j_params)) {
+                    $options = $p_params;
                 }
+            } catch (Exception $e) {
+            }
+        }
+        return $options;
+    }
+
+    /**
+     * @param $w_params array|mixed default widget params
+     * @param $j_params array|mixed extracted widget params
+     * @param $w_name string Widget name
+     * @return array|mixed
+     */
+    public function replaceOneWidget($w_params, $j_params, $w_name)
+    {
+        if (gettype($w_params) == 'array') {
+            //overload widget params by json
+            $w_params = ArrayHelper::merge($w_params, $j_params);
+            if ($data = \Yii::createObject($w_params)) {
+                return $data->run();
+            }
+        }
+        if (gettype($w_params) == 'string') {
+            return $w_params;
+        }
+        if ($w_params instanceof \Closure) {
+            return call_user_func($w_params, $w_name, $j_params);
+        }
+    }
+
+    /**
+     * Replace all widgets one-by-one.
+     * NOTE: avoid much numbers of widgets
+     */
+    public function replaceAllWidgets()
+    {
+        foreach ($this->widgets as $w_name => $options) {
+            $this->html = preg_replace_callback("/{$this->beginTag}$w_name.*?{$this->endTag}/s", function ($match) use ($w_name) {
+                $item = current($match);
+                $j_params = $this->extractJsonParams($w_name, $item);
+                $w_params = ArrayHelper::getValue($this->widgets, $w_name);
+                return $this->replaceOneWidget($w_params, $j_params, $w_name);
             }, $this->html);
         }
     }
 
+    /**
+     * @return string
+     */
     public function run()
     {
-        $this->_replaceWidgets();
+        $this->replaceAllWidgets();
         return $this->html;
     }
 
